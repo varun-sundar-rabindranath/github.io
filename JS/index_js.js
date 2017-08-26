03bbcea976bcb46e0abd0248b48a29ea16278fb6
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2017-08-26 18:47:25 -0400

    [try-canvas] Add rects to canvas

--- JS/index_js.js
+++ JS/index_js.js
@@ -3,5 +3,12 @@ $(document).ready(
     console.log("Yeah !! I am ready !!");
     var canvas = document.getElementById('tutorial');
     var canvas_ctx = canvas.getContext('2d');
+
+    canvas_ctx.fillStyle = 'rgb(200, 0, 0)';
+    canvas_ctx.fillRect(10, 10, 50, 50);
+
+
+    canvas_ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    canvas_ctx.fillRect(30, 30, 50, 50);
   }
 );
