0c8fb03b228fa7fbaaea6e9dc82534e820bf3606
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2017-08-26 19:56:20 -0400

    [try-canvas] Add drawArc

--- JS/index_js.js
+++ JS/index_js.js
@@ -10,6 +10,8 @@ $(document).ready(
 
     //drawTriangle(ctx);
 
+    drawArc(ctx);
+
   }
 );
 
@@ -27,4 +29,12 @@ function drawRects(ctx) {
   // Draw and fill rectangle
   ctx.fillStyle = 'rgb(200, 0, 0)';
   ctx.fillRect(10, 10, 50, 50);
+}
+
+function drawArc(ctx) {
+  // Draw arc
+  ctx.beginPath();
+  ctx.moveTo(75, 75);
+  ctx.arc(75, 75, 50, 0, Math.PI * 1.5, false);
+  ctx.fill();
 }
