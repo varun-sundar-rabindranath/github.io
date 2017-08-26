c90462248c5c6dd3cf5d5b924ed19ca4892ae408
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2017-08-26 19:05:29 -0400

    [try-canvas] Draw a triangle

--- JS/index_js.js
+++ JS/index_js.js
@@ -1,14 +1,20 @@
 $(document).ready(
   function() {
     console.log("Yeah !! I am ready !!");
+
+    // Get canvas and initialize context
     var canvas = document.getElementById('tutorial');
-    var canvas_ctx = canvas.getContext('2d');
+    var ctx    = canvas.getContext('2d');
 
-    canvas_ctx.fillStyle = 'rgb(200, 0, 0)';
-    canvas_ctx.fillRect(10, 10, 50, 50);
+    //ctx.fillStyle = 'rgb(200, 0, 0)';
+    //ctx.fillRect(10, 10, 50, 50);
 
+    // Draw triangle
+    ctx.beginPath();
+    ctx.moveTo(75,  50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
 
-    canvas_ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    canvas_ctx.fillRect(30, 30, 50, 50);
   }
 );
