e60133732070ab76997a77c4505ac7682b20b286
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2017-08-26 18:42:02 -0400

    [try_canvas] Access canvas in javascript

--- JS/index_js.js
+++ JS/index_js.js
@@ -1,5 +1,7 @@
 $(document).ready(
   function() {
     console.log("Yeah !! I am ready !!");
+    var canvas = document.getElementById('tutorial');
+    var canvas_ctx = canvas.getContext('2d');
   }
 );
